d4d247e0cbd2612e61e6a284835f85a645d7f567
Shawn Roller <sroller@techstyle.com>
2020-03-31 12:40:41 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import OktaAuth from "@okta/okta-auth-js";
+import { config, login } from './config';
 
 function App() {
+
+  const [accessToken, setAccessToken] = useState('');
+  const [idToken, setIDToken] = useState('');
+
+  const authenticate = async () => {
+    console.log('logging in');
+
+    const authClient = new OktaAuth(config);
+    const transaction = await authClient.signIn(login);
+    const { sessionToken } = transaction;
+    const { tokens } = await authClient.token.getWithoutPrompt({ sessionToken });
+    setAccessToken(tokens.accessToken.value);
+    setIDToken(tokens.idToken.value);
+    console.log(`Access token: ${tokens.accessToken.value}`);
+    console.log(`ID token: ${tokens.idToken.value}`);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <button onClick={authenticate} >authenticate</button>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Access Token:
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>
+          {accessToken}
+        </p>
+        <p>
+          ID Token:
+        </p>
+        <p>
+          {idToken}
+        </p>
       </header>
     </div>
   );
